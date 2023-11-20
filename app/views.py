0a191c6f36ed335f88cd6960286e0c674e7a5d9f
Hong Gayoung <thehd9891@naver.com>
2023-11-20 19:47:15 +0900

    제발

--- app/views.py
+++ app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
+from .models import Major1, Major2, Tag, Member
+
 # Create your views here.
+
+def main(request):
+    return render(
+        request,
+        'main.html',
+    )
+
+def join(request):
+    return
+
+def login(request):
+    return
+
+def list(request):
+    return
+
+def mypage(request):
+    return
+
+def myfriends(request):
+    return
+
+def message(request):
+    return
