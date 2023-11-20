2cd1da93e6f1ad0bb901182a9c77179f2329924a
Yesungg <90364714+Yesungg@users.noreply.github.com>
2023-11-20 19:27:16 +0900

    urls settings

--- app/views.py
+++ app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
+from .models import Major1, Major2, Tag, Member
+
 # Create your views here.
+
+def main(request):
+    return render(
+        request,
+        '/main.html',
+
+    )
+
+def join():
+    return
+
+def login():
+    return
+
+def list():
+    return
+
+def mypage():
+    return
+
+def myfriends():
+    return
+
+def message():
+    return
