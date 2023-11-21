ea40021a1e82b741d4a9503a765cf149947d4faf
Yesungg <90364714+Yesungg@users.noreply.github.com>
2023-11-21 21:41:57 +0900

    urls setting

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('main', views.main),
+    path('join', views.join),
+    path('login', views.login),
+    path('list', views.list),
+    path('mypage', views.mypage),
+    path('myfriends', views.myfriends),
 ]
